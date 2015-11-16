adb4b30f173610f3deea2bd0b3da74f170cb37cf
ack8006 <takata.alex@gmail.com>
2015-11-15 23:34:31 -0500

    test generation updates

--- solver.py
+++ solver.py
@@ -1,6 +1,7 @@
 import random
 from collections import deque
 import itertools
+import copy
 
 
 class Board(object):
@@ -35,7 +36,7 @@ class Solver(object):
                 else None for x in ['A','K','Q']}
 
     def solver_logic(self):
-        self.board.display_board()
+        #self.board.display_board()
         board_loc = self.get_locations()
         if self.board.get_board_length() == 3:
             if board_loc['Q'] == 0 and board_loc['K'] == 1:
@@ -55,12 +56,11 @@ class Solver(object):
                 self.board.move_piece_right(board_loc[top_card])
 
 def run_program(locations):
-    print locations
+    #print locations
     board = Board(locations)
     s = Solver(board)
-    for x in xrange(25):
+    for x in xrange(100):
         if s.solver_logic():
-            print 'Solved'
             return True
     return False
 
@@ -78,36 +78,36 @@ def gen_all_tests():
                     map(make_boards, itertools.permutations(['A','K','Q'],3)))
     return boards
 
+def get_wambo_combos():
+    return [[3,0,0],[2,1,0],[1,2,0],[1,1,1]]
+
 def make_boards(perm):
-    boards = make_threes(perm)
-    boards += make_flat(perm)
-    boards += make_twos(perm)
-    return boards
+    return reduce(lambda a,x: a+x, (gen_boards(list(perm), combo) for
+                                    combo in get_wambo_combos()))
 
-def make_threes(perm):
-    boards = []
-    for x in xrange(3):
-        board = (deque(['_']),deque(['_']),deque(['_']))
-        for y in perm:
-            board[x].appendleft(y)
-        boards.append(board)
-    return boards
+def gen_boards(perm, combo):
+    board = [deque(['_']),deque(['_']),deque(['_'])]
+    for ind, num_com in enumerate(combo):
+        while num_com:
+            board[ind].appendleft(perm.pop())
+            num_com -= 1
+    # if 1,1,1 no need to rotate
+    if all(map(lambda x: len(x)==2, board)):
+        #return list of board, because later flatten
+        return [board]
+    return [rotate_boards(board, x) for x in xrange(3)]
 
-def make_flat(perm):
-    return [tuple(map(lambda x:deque([x,'_']), perm))]
+def rotate_boards(board, n):
+    return board[-n:] + board[:-n]
 
-def make_twos(perm):
-    boards = []
-    for x in xrange(3):
-        board = (deque(['_']),deque(['_']),deque(['_']))
-        board[x].appendleft(perm[0])
-        board[x].appendleft(perm[1])
-        board[(x+1)%3].appendleft(perm[2])
-        boards.append(board)
-    return boards
 
 if __name__ == '__main__':
     for test in gen_all_tests():
-        result = run_program(test)
-        if not result:
-            print 'FAIL'
+        print test
+        if run_program(copy.deepcopy(test)):
+            print 'Solved'
+        else:
+            print 'Failed'
+    print
+    #for x in gen_all_tests():
+    #    print x
